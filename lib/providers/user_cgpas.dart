c3585880087a27b5b935ef560e3b6478b4ef34c1
SemajCode <mfonjames23@gmail.com>
2023-08-31 20:10:42 +0100

    Code Updated

---
+++ lib/providers/user_cgpas.dart
@@ -0,0 +1,112 @@
+// import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gpa_calculator/model/cgpas.dart';
+import 'package:gpa_calculator/model/course.dart';
+import 'package:gpa_calculator/model/gpa.dart';
+// import 'package:flutter/material.dart';
+// import 'package:gpa_calculator/screens/add_cgpa.dart';
+
+class UserCgpaNotifier extends StateNotifier<List<Cgpas>> {
+  UserCgpaNotifier() : super(const []);
+
+  void addCgpa() {
+    final newCgpa = Cgpas(
+      name: 'CGPA${state.length + 1}',
+      gpa: [
+        Gpa(
+          courses: [],
+          semester: 'First semester',
+          session: 'Session 1',
+        ),
+      ],
+    );
+    state = [newCgpa, ...state];
+  }
+
+  int getNoOfCourses(cgpaIndex) {
+    int noOfCourse = 0;
+    final gpaList = state[cgpaIndex].gpa;
+    for (var element in gpaList) {
+      noOfCourse += element.courses.length;
+    }
+
+    if (noOfCourse == 0) {
+      state.remove(state[cgpaIndex]);
+    }
+    return noOfCourse;
+  }
+
+  int getData(cgpaIndex, data) {
+    int value = 0;
+    final gpaList = state[cgpaIndex].gpa;
+    for (var element in gpaList) {
+      int coursesLength = element.courses.length;
+      int i = 0;
+      while (i < coursesLength) {
+        if (data == 'point') {
+          value += element.courses[i].point;
+        } else {
+          value += element.courses[i].unit;
+        }
+        ++i;
+      }
+    }
+    return value;
+  }
+
+  int getNoOfSessions(cgpaIndex) {
+    int noOfSessions = 0;
+    final gpaListLength = state[cgpaIndex].gpa.length;
+    for (var i = 0; i < gpaListLength; i++) {
+      if (i <= 1) {
+        noOfSessions = 1;
+      } else if (i <= 3) {
+        noOfSessions = 2;
+      } else if (i <= 5) {
+        noOfSessions = 3;
+      } else if (i <= 7) {
+        noOfSessions = 4;
+      } else if (i <= 9) {
+        noOfSessions = 5;
+      } else if (i <= 11) {
+        noOfSessions = 6;
+      } else if (i <= 3) {
+        noOfSessions = 7;
+      } else {
+        noOfSessions = 8;
+      }
+    }
+    return noOfSessions;
+  }
+
+  void createGpa(cgpaIndex, semester, session) {
+    state[cgpaIndex].gpa.add(
+          Gpa(
+            courses: [],
+            semester: semester,
+            session: session,
+          ),
+        );
+  }
+
+  void addCourse(cgpaIndex, currentGpaIndex, Course course) {
+    state[cgpaIndex].gpa[currentGpaIndex].courses.add(course);
+  }
+
+  void removeCourse(Course course, cgpaIndex, currentGpaIndex) {
+    state[cgpaIndex].gpa[currentGpaIndex].courses.remove(course);
+  }
+
+  void insertCourse(Course course, cgpaIndex, currentGpaIndex) {
+    final courseIndex =
+        state[cgpaIndex].gpa[currentGpaIndex].courses.indexOf(course);
+    state[cgpaIndex].gpa[currentGpaIndex].courses.insert(courseIndex, course);
+  }
+
+  void editCourse(Course course, index, cgpaIndex, currentGpaIndex) {
+    state[cgpaIndex].gpa[currentGpaIndex].courses[index] = course;
+  }
+}
+
+final userCgpaProvider = StateNotifierProvider<UserCgpaNotifier, List<Cgpas>>(
+    (ref) => UserCgpaNotifier());
